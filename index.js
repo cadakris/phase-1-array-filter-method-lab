700eee71cea3dcdddc21fe79e46eb12884ea46a2
Kristen <kriscadacio@gmail.com>
2022-02-27 08:16:56 -0500

    commit tree 16ec8720dcbf81de463942d431f9ccf1da17bd81

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution here
+
+const { ResourceLoader } = require("jsdom");
+
+// create function called findMatching with 2 params - array of names and a string
+// returns a matching list of drivers
+// need to use tolowercase to make the names the same
+
+// function findMatching (arr, string) {
+//     return arr.filter(driver => string.toLowerCase() === driver.toLowerCase())
+// }
+
+
+// create a function called fuzzymatch 
+// parameters are 1 - an array of drivers names and 2 a string
+// I need to compare the firstletters of the drivers to the string 
+// then I need to return the actual name
+
+// function fuzzyMatch (arr, string) {
+//     return arr.filter(driver => driver.startsWith(string))
+// }
+
+// create a function called matchName 
+// takes two parameters string and an array of objects
+// use filter method to iterate through the array of objects whose name key matches the provided string
+
+// function matchName (arrOfDrivers,string) {
+//     return arrOfDrivers.filter(driver => driver.name === string)
+// }
+
+
+function findMatching (arr, string) {
+    return arr.filter(driver => string.toLowerCase() === driver.toLowerCase())
+}
+
+function fuzzyMatch (arr, string) {
+    
+}
+
